4cf152d3136784345923085163db10cc8999a237
Reethu Thota <reethu.thota@gmail.com>
2023-08-07 19:27:26 +0530

    Update findAll.c, Lab-1

--- lab1/findAll.c
+++ lab1/findAll.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 
 int findSum(int numParams, char * argv[]);
+int findMin(int numParams, char * argv[]);
+int findMax(int numParams, char * argv[]);
+int findAvg(int numParams, char * argv[]);
 
 int main(int argc, char *argv[])
 {
     printf("Find the sum of numbers entered by the user!!!\n");
 
-    if(argc < 2){
+    if(argc < 3){
         printf("Error: Insufficient parameters!!!\n");
         printf("Give at least one or more valid integer numbers as parameters.\n");
         return -1;
     }
 
-    int numParams = argc -1;
+    int numParams = argc - 2;
     int sum = 0;
 
 
@@ -21,7 +24,21 @@ int main(int argc, char *argv[])
         printf(" argv[%d] = %s\n", i, argv[i]);
     }
 
-    sum = findSum(numParams, argv);
+
+    if(*argv[1] == 'SUM') {
+        sum = findSum(numParams, argv);
+    } 
+    else if(*argv[1] == 'MIN') {
+        sum = findSum(numParams, argv);
+    }
+    else if(*argv[1] == 'MAX') {
+        sum = findSum(numParams, argv);
+    }
+    else if(*argv[1] == 'AVG') {
+        sum = findSum(numParams, argv);
+    }
+
+    
 
     printf("The sum of numbers given is: %d\n", sum);
 
@@ -34,7 +51,7 @@ int findSum(int numParams, char * argv[]){
     int sum = 0;
     int num;
 
-    pParam = argv[1];
+    pParam = argv[2];
 
    for(int i = 1; i <= numParams; i++ ){
         pParam = argv[i];  // Copy the pointer to the first parameter passed
@@ -43,4 +60,72 @@ int findSum(int numParams, char * argv[]){
         printf("pParam = %s, sum = %d\n", pParam, sum);
    }
     return sum;
+}
+
+int findMin(int numParams, char * argv[]){
+
+   char *pParam;
+   int min = 0;
+   int num;
+
+   pParam = argv[1];
+
+    for(int i = 1; i <= numParams; i++ ){
+        pParam = argv[i];  // Copy the pointer to the first parameter passed
+        num = atoi(pParam);
+        if (i == 1) {
+            min = num;
+        }
+        else {
+            if(min > num) {
+                min = num;
+            }
+        }
+        printf("pParam = %s, min = %d\n", pParam, min);
+   }
+    return min;
+}
+
+int findMax(int numParams, char * argv[]){
+
+   char *pParam;
+   int max = 0;
+   int num;
+
+   pParam = argv[1];
+
+    for(int i = 1; i <= numParams; i++ ){
+        pParam = argv[i];  // Copy the pointer to the first parameter passed
+        num = atoi(pParam);
+        if (i == 1) {
+            max = num;
+        }
+        else {
+            if(max < num) {
+                max = num;
+            }
+        }
+        printf("pParam = %s, max = %d\n", pParam, max);
+   }
+    return max;
+}
+
+int findAvg(int numParams, char * argv[]){
+
+   char *pParam;
+   int avg = 0;
+   int sum = 0;
+   int num;
+
+   pParam = argv[1];
+
+    for(int i = 1; i <= numParams; i++ ){
+        pParam = argv[i];  // Copy the pointer to the first parameter passed
+        num = atoi(pParam);
+        sum += num;
+        printf("pParam = %s, sum = %d\n", pParam, sum);
+    }
+    printf("numParams = %d\n", numParams);
+    avg = sum/numParams;
+    return avg;
 }
