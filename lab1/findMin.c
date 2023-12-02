fafb938360f0ac2fa5e2b7bfe211a970e3907c28
reethuthota <reethu.thota@gmail.com>
2023-12-02 09:52:08 +0530

    Revert "Delete lab1 directory"
    This reverts commit 8ad5435ff9e4166e5cc5ae302ba3759bf4d77540.

---
+++ lab1/findMin.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findMin(int numParams, char * argv[]);
+
+int main(int argc, char *argv[])
+{
+    printf("Find the sum of numbers entered by the user!!!\n");
+
+    if(argc < 2){
+        printf("Error: Insufficient parameters!!!\n");
+        printf("Give at least one or more valid integer numbers as parameters.\n");
+        return -1;
+    }
+
+    int numParams = argc -1;
+    int min = 0;
+
+
+    for(int i = 0; i < argc; i++){
+        printf(" argv[%d] = %s\n", i, argv[i]);
+    }
+
+    min = findMin(numParams, argv);
+
+    printf("The minimum of numbers given is: %d\n", min);
+
+    return 0;
+}
+
+int findMin(int numParams, char * argv[]){
+
+   char *pParam;
+   int min = 0;
+   int num;
+
+   pParam = argv[1];
+
+    for(int i = 1; i <= numParams; i++ ){
+        pParam = argv[i];  // Copy the pointer to the first parameter passed
+        num = atoi(pParam);
+        if (i == 1) {
+            min = num;
+        }
+        else {
+            if(min > num) {
+                min = num;
+            }
+        }
+        printf("pParam = %s, min = %d\n", pParam, min);
+   }
+    return min;
+}
